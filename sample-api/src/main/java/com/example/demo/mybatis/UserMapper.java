bf914d0aebe5f8711e613b4b8fdbf83ec25e548d
WHITE-GRINT\Kazuki <kzktcy0610@gmail.com>
2021-04-18 19:27:36 +0900

    MyBatisのxml化

--- sample-api/src/main/java/com/example/demo/mybatis/UserMapper.java
+++ sample-api/src/main/java/com/example/demo/mybatis/UserMapper.java
@@ -13,43 +13,20 @@ import com.example.demo.model.User;
 import com.example.demo.model.form.UserPostForm;
 import com.example.demo.model.form.UserPutForm;
 
-@Mapper
 public interface UserMapper {
-
-	@Select("SELECT * FROM user;")
-	public List<User> getUsers() throws DataAccessException;
 	
-	@Select("SELECT * FROM user WHERE name like #{name}")
+	public List<User> getUsers() throws DataAccessException;
+
 	public List<User> getUsersByName(String name) throws DataAccessException;
 	
-	@Select("SELECT * FROM user WHERE id = #{id}")
 	public User getUserById(String id) throws DataAccessException;
 	
-	@Insert("INSERT INTO user("
-			+ "name ,"
-			+ "age ,"
-			+ "company) "
-			+ "VALUES("
-			+ " #{name},"
-			+ " #{age},"
-			+ " #{company})")
 	public void insertUser(UserPostForm postForm) throws DataAccessException;
 	
-	@Select("SELECT MAX(id) "
-			+ "FROM user")
 	public String getLastUserId() throws DataAccessException;
 	
-	@Update("UPDATE user "
-			+ "SET name = #{name} ,"
-			+ "age = #{age} ,"
-			+ "company = #{company} "
-			+ "WHERE id = #{id}")
 	public void updateUser(String id, UserPutForm putForm) throws DataAccessException;
 	
-	@Delete("DELETE FROM user "
-			+ "WHERE id = #{id}")
 	public void deleteUserById(String id) throws DataAccessException;
-	
-
 	
 }
--- sample-api/src/main/resources/data.sql
+++ sample-api/src/main/resources/data.sql
@@ -1,5 +1,8 @@
 INSERT INTO user (id ,name, age, company)
-VALUES(1, 'Iwasaki', 28, 'BlackCompany');
+VALUES(1, 'Iwasaki', 29, 'Company1');
 
 INSERT INTO user (id ,name, age, company)
-VALUES(2, 'Ochi', 28, 'WhiteCompany');
+VALUES(2, 'Ochi', 28, 'Company2');
+
+INSERT INTO user (id ,name, age, company)
+VALUES(3, 'Ohnuki', 28, 'Company2');
